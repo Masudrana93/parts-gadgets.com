b9ea26d11a62f44e241d31b06242e7a353d9a5b0
Masud Rana <masudrana730@gmail.com>
2025-07-30 00:19:19 +0600

    created top sales slider

--- js/swiper.js
+++ js/swiper.js
@@ -6,12 +6,25 @@ var swiper = new Swiper(".mySwiper", {
       },
     });
 
-//for Section-1 == Shop by category Slider
+//for Section-2 == Shop by category Slider
 var swiper = new Swiper(".mySwiper2", {
       slidesPerView: 5,
       spaceBetween: 30,
       navigation: {
         nextEl: ".category-button-next2",
         prevEl: ".category-button-prev2",
+      },
+    });
+
+//for Section-3 == Top Sales Slider
+    var swiper = new Swiper(".mySwiper3", {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".top-sales-button-next",
+        prevEl: ".top-sales-button-prev",
       },
     });
