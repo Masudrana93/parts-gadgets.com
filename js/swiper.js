12fd557af06c4aeb1d7061cc8c8c4a1b3f67dd6d
Masud Rana <masudrana730@gmail.com>
2025-07-29 20:43:45 +0600

    created category slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,17 @@
-
+//for Section-1 == main slider
 var swiper = new Swiper(".mySwiper", {
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+    });
+
+//for Section-1 == Shop by category Slider
+var swiper = new Swiper(".mySwiper2", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".category-button-next2",
+        prevEl: ".category-button-prev2",
       },
     });
