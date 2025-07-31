cc8dc18fc7427827184a72f5d9804ab3cd47ae75
Masud Rana <masudrana730@gmail.com>
2025-07-31 20:34:58 +0600

    tractor parts slider

--- js/swiper.js
+++ js/swiper.js
@@ -19,12 +19,32 @@ var swiper = new Swiper(".mySwiper2", {
 //for Section-3 == Top Sales Slider
     var swiper = new Swiper(".mySwiper3", {
       slidesPerView: 4,
-      grid: {
-        rows: 2,
-      },
+      // grid: {
+      //   rows: 2,
+      // },
       spaceBetween: 30,
       navigation: {
         nextEl: ".top-sales-button-next",
         prevEl: ".top-sales-button-prev",
+      },
+    });
+
+    //for Section-4 == Car Parts Slider
+    var swiper = new Swiper(".mySwiper4", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".car-parts-button-next",
+        prevEl: ".car-parts-button-prev",
+      },
+    });
+
+     //for Section-4 == Car Parts Slider
+    var swiper = new Swiper(".mySwiper5", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".tractor-parts-button-next",
+        prevEl: ".tractor-parts-button-prev",
       },
     });
