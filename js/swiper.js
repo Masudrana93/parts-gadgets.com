057251e2fb3807d33d609bd35760009a3fdc36a0
Masud Rana <masudrana730@gmail.com>
2025-07-31 23:14:30 +0600

    deal of the day

--- js/swiper.js
+++ js/swiper.js
@@ -152,6 +152,19 @@ var swiper = new Swiper(".mySwiper2", {
     });
 
 
+    var swiper = new Swiper(".mySwiper7", {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+
     
   
 
